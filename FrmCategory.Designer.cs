ed8dfe1b30df75f6623305f4051e0b070fc5c302
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-11 17:06:36 +0300

    Add files via upload

---
+++ FrmCategory.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Project10_PostgreSQLToDoListApp
+{
+    partial class FrmCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetById = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtCatName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnList = new System.Windows.Forms.Button();
+            this.txtCatId = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DGVCategory = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCategory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetById
+            // 
+            this.btnGetById.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGetById.Location = new System.Drawing.Point(647, 135);
+            this.btnGetById.Name = "btnGetById";
+            this.btnGetById.Size = new System.Drawing.Size(142, 39);
+            this.btnGetById.TabIndex = 27;
+            this.btnGetById.Text = "All List";
+            this.btnGetById.UseVisualStyleBackColor = true;
+            this.btnGetById.Click += new System.EventHandler(this.btnGetById_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.Location = new System.Drawing.Point(647, 91);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(142, 39);
+            this.btnDelete.TabIndex = 26;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.Location = new System.Drawing.Point(647, 44);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(142, 39);
+            this.btnUpdate.TabIndex = 25;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCreate.Location = new System.Drawing.Point(465, 85);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(142, 39);
+            this.btnCreate.TabIndex = 24;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtCatName
+            // 
+            this.txtCatName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCatName.Location = new System.Drawing.Point(175, 83);
+            this.txtCatName.Name = "txtCatName";
+            this.txtCatName.Size = new System.Drawing.Size(263, 28);
+            this.txtCatName.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(29, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 22);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Category Name:";
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnList.Location = new System.Drawing.Point(465, 42);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(142, 39);
+            this.btnList.TabIndex = 21;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // txtCatId
+            // 
+            this.txtCatId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtCatId.Location = new System.Drawing.Point(175, 42);
+            this.txtCatId.Name = "txtCatId";
+            this.txtCatId.Size = new System.Drawing.Size(263, 28);
+            this.txtCatId.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(62, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 22);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Category Id:";
+            // 
+            // DGVCategory
+            // 
+            this.DGVCategory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DGVCategory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVCategory.Location = new System.Drawing.Point(33, 140);
+            this.DGVCategory.Name = "DGVCategory";
+            this.DGVCategory.RowHeadersWidth = 51;
+            this.DGVCategory.RowTemplate.Height = 24;
+            this.DGVCategory.Size = new System.Drawing.Size(574, 319);
+            this.DGVCategory.TabIndex = 28;
+            // 
+            // FrmCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(822, 486);
+            this.Controls.Add(this.DGVCategory);
+            this.Controls.Add(this.btnGetById);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtCatName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.txtCatId);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCategory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmCategory";
+            this.Load += new System.EventHandler(this.FrmCategory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVCategory)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetById;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtCatName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.TextBox txtCatId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView DGVCategory;
+    }
+}
---
+++ FrmCategory.cs
@@ -0,0 +1,116 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project10_PostgreSQLToDoListApp
+{
+    public partial class FrmCategory: Form
+    {
+        public FrmCategory()
+        {
+            InitializeComponent();
+        }
+        string connectionString = ("Server=localhost;port=5432;Database=DbProject10ToDoApp;user Id=postgres;Password=Gunes123");
+
+        void CategoryList()
+        {
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string query = "Select * From categories order by CategoryId";
+            var command = new NpgsqlCommand(query, conn);
+            var adapter = new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            DGVCategory.DataSource = dt;
+        }
+        private void FrmCategory_Load(object sender, EventArgs e)
+        {
+            CategoryList();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            CategoryList();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string query = "insert into Categories (CategoryName) values (@categoryName)";
+            using (var command = new NpgsqlCommand(query,conn))
+            {
+                command.Parameters.AddWithValue("@categoryName",txtCatName.Text);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Category Added Successfully");
+                CategoryList();
+            }
+            conn.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCatId.Text);
+
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string query = "Delete From Categories Where CategoryId=@categoryId";
+            using (var command = new NpgsqlCommand(query,conn))
+            {
+                command.Parameters.AddWithValue("@categoryId",id);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Category Deleted Successfully!");
+                CategoryList();
+            }
+            conn.Close();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtCatId.Text);
+            string categoryName = txtCatName.Text;
+
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string query = "Update Categories Set CategoryName=@categoryName where CategoryId=@categoryId";
+            using (var command = new NpgsqlCommand(query,conn))
+            {
+                command.Parameters.AddWithValue("@categoryId",id);
+                command.Parameters.AddWithValue("@categoryName",categoryName);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Category Updated Successfully");
+                CategoryList();
+            }
+            conn.Close();
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            // ID ye göre getirme işlemi
+            int id = int.Parse(txtCatId.Text);
+            using (var connection=new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Select * From Categories where CategoryId=@categoryId";
+                using (var command =new NpgsqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@categoryId", id);
+                    using (var adapter = new NpgsqlDataAdapter(command))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        DGVCategory.DataSource = dt;
+                    }
+                }
+                connection.Close();
+            }
+        }
+    }
+}

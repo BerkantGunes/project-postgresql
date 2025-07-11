8c72a1d5f309f13844b01ec2aaa1436bfb69872d
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-11 17:06:54 +0300

    Add files via upload

---
+++ FrmToDoListApp.cs
@@ -0,0 +1,34 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project10_PostgreSQLToDoListApp
+{
+    public partial class FrmToDoListApp: Form
+    {
+        public FrmToDoListApp()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = ("Server=localhost;port=5432;Database=DbProject10ToDoApp;user Id=postgres;Password=Gunes123");
+        private void FrmToDoListApp_Load(object sender, EventArgs e)
+        {
+            var conn = new NpgsqlConnection(connectionString);
+            conn.Open();
+            string query = "Select * From todolists";
+            var command = new NpgsqlCommand(query,conn);
+            var adapter = new NpgsqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            DGVList.DataSource = dt;
+        }
+    }
+}
